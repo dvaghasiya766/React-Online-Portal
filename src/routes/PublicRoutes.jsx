7d0fe164e6972af0d1665411b034164bed57e69e
dvaghasiya766 <dvaghasiya766@rku.ac.in>
2025-04-03 17:58:04 +0530

    Initial commit or your message here

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -19,10 +19,10 @@ const PublicRoutes = () => {
     <MainLayout>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* <Route path="/contact" element={<Contact />} /> */}
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </MainLayout>
   );
